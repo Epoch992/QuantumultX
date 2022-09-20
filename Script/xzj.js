4457affc16eaeb58a0a3e9e0c8291b9ed1261471
Epoch992 <113267395+Epoch992@users.noreply.github.com>
2022-09-21 06:25:01 +0800

    Update xzj.js

--- Script/xzj.js
+++ Script/xzj.js
@@ -14,7 +14,7 @@ var obj = {
   "environment" : "Production",
   "receipt" : {
     "receipt_type" : "Production",
-    "app_item_id" : 1016366447,
+    "app_item_id" : 0,
     "receipt_creation_date" : "2022-09-14 02:48:29 Etc/GMT",
     "bundle_id" : "net.shinyfrog.bear-iOS",
     "original_purchase_date" : "2020-07-06 01:50:15 Etc/GMT",
@@ -25,21 +25,21 @@ var obj = {
         "expires_date" : "2099-12-31 23:59:59 Etc/GMT",
         "expires_date_pst" : "2099-12-31 23:59:59 America/Los_Angeles",
         "is_in_intro_offer_period" : "false",
-        "transaction_id" : "430000659572416",
+        "transaction_id" : "0",
         "is_trial_period" : "true",
-        "original_transaction_id" : "430000659572416",
+        "original_transaction_id" : "0",
         "purchase_date" : "2020-07-06 01:53:42 Etc/GMT",
         "product_id" : "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis",
         "original_purchase_date_pst" : "2020-07-05 18:53:43 America/Los_Angeles",
         "in_app_ownership_type" : "PURCHASED",
         "original_purchase_date_ms" : "1594000423000",
-        "web_order_line_item_id" : "430000254060883",
+        "web_order_line_item_id" : "0",
         "expires_date_ms" : "1596678822000",
         "purchase_date_pst" : "2020-07-05 18:53:42 America/Los_Angeles",
         "original_purchase_date" : "2020-07-06 01:53:43 Etc/GMT"
       }
     ],
-    "adam_id" : 1016366447,
+    "adam_id" : 0,
     "receipt_creation_date_pst" : "2022-09-13 19:48:29 America/Los_Angeles",
     "request_date" : "2022-09-14 02:50:50 Etc/GMT",
     "request_date_pst" : "2022-09-13 19:50:50 America/Los_Angeles",
@@ -50,7 +50,7 @@ var obj = {
     "original_purchase_date_ms" : "1594000215000",
     "receipt_creation_date_ms" : "1663123709000",
     "original_application_version" : "8457",
-    "download_id" : 83067705285576
+    "download_id" : 0
   },
   "pending_renewal_info" : [
     {
@@ -58,7 +58,7 @@ var obj = {
       "product_id" : "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis",
       "is_in_billing_retry_period" : "0",
       "auto_renew_product_id" : "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis",
-      "original_transaction_id" : "430000659572416",
+      "original_transaction_id" : "0",
       "auto_renew_status" : "0"
     }
   ],
@@ -70,17 +70,17 @@ var obj = {
       "expires_date" : "2099-12-31 23:59:59 Etc/GMT",
       "expires_date_pst" : "2099-12-31 23:59:59 America/Los_Angeles",
       "is_in_intro_offer_period" : "false",
-      "transaction_id" : "430000659572416",
+      "transaction_id" : "0",
       "is_trial_period" : "true",
-      "original_transaction_id" : "430000659572416",
+      "original_transaction_id" : "0",
       "purchase_date" : "2020-07-06 01:53:42 Etc/GMT",
       "product_id" : "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis",
       "original_purchase_date_pst" : "2020-07-05 18:53:43 America/Los_Angeles",
       "in_app_ownership_type" : "PURCHASED",
       "subscription_group_identifier" : "20354566",
       "original_purchase_date_ms" : "1594000423000",
-      "web_order_line_item_id" : "430000254060883",
-      "expires_date_ms" : "9996678822000",
+      "web_order_line_item_id" : "0",
+      "expires_date_ms" : "4102415999",
       "purchase_date_pst" : "2020-07-05 18:53:42 America/Los_Angeles",
       "original_purchase_date" : "2020-07-06 01:53:43 Etc/GMT"
     }
