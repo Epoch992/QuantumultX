9557a06c185ea6b3756ce71a2bedb349f0697b0c
Epoch992 <113267395+Epoch992@users.noreply.github.com>
2022-10-08 19:53:25 +0800

    Update NicegramPro.js

--- Script/NicegramPro.js
+++ Script/NicegramPro.js
@@ -1,6 +1,9 @@
 /****
-Nicegram: UnlimitedText+解锁订阅
+Nicegram解锁订阅
 
 [rewrite_local]
 
 https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/Epoch992/QuantumultX/QX/Script/Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
