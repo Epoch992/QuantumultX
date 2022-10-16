0962bddaac9c30afaa653325511b30ca2da82ed7
Epoch992 <113267395+Epoch992@users.noreply.github.com>
2022-10-16 08:02:14 +0800

    Update and rename STSQ.js to stsq.js

--- Script/stsq.js
+++ Script/stsq.js
@@ -1,6 +1,6 @@
 /***
 [rewrite_local]
-http:\/\/wallpaper\.soutushenqi\.com\/api\/v1\/account\/token url script-response-body https://raw.githubusercontent.com/Epoch992/QuantumultX/QX/Script/STSQ.js
+http:\/\/wallpaper\.soutushenqi\.com\/api\/v1\/account\/token url script-response-body https://raw.githubusercontent.com/Epoch992/QuantumultX/QX/Script/stsq.js
 hostname = wallpaper.soutushenqi.com
 ***/
 var body = JSON.parse($response.body);
