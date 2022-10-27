142b6d9d372cc70e53e6349c42fbec4fe8a6d915
Epoch992 <113267395+Epoch992@users.noreply.github.com>
2022-10-28 00:50:23 +0800

    Update JDCK.js

--- Script/JDCK.js
+++ Script/JDCK.js
@@ -1,3 +1,9 @@
+#!name=JDCK
+#!desc=JDCK
+ 
+[Script] 
+JDwskey = type=http-response,pattern=^https:\/\/api\.m\.jd\.com\/client\.action(\.+)newUserInfo,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Epoch992/QuantumultX/QX/Script/JDCK.js,script-update-interval=0 
+
 [rewrite_local]
 
 https://api.m.jd.com/client.action(.+)newUserInfo url script-response-body https://raw.githubusercontent.com/Epoch992/QuantumultX/QX/Script/JDwskey.js
