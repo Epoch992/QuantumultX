bdb2df4809208dd19c8450b7477167ed2b5357ca
Epoch992 <113267395+Epoch992@users.noreply.github.com>
2022-10-28 00:20:06 +0800

    Update JDwskey.js

--- Script/JDwskey.js
+++ Script/JDwskey.js
@@ -9,9 +9,7 @@ var CookieValue = ck.match(/wskey=.+?;/)
 
 data=JSON.parse($response.body)
     data = data.userInfoSns.unickName
-    url = '%E4%BD%99%E6%B8%A9wskey%E8%8E%B7%E5%8F%96%E6%8F%90%E9%86%92%E6%82%A8'
-
-
+    url = 'Wskey%E8%8E%B7%E5%8F%96%E5%A6%82%E4%B8%8B'
 
 console.log(ck)
 $notify(decodeURI(url), "京东wskey获取成功", "pin="+data+";"+CookieValue);
