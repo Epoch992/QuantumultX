849c2d1299d1a53c10ef9de8407fb73245436a7d
Epoch992 <113267395+Epoch992@users.noreply.github.com>
2022-10-28 13:51:00 +0800

    Update JDCK.js

--- Script/JDCK.js
+++ Script/JDCK.js
@@ -1,3 +1,5 @@
+/****
+#京东CK获取
 [rewrite_local]
 
 ^https:\/\/api\.m\.jd\.com\/client\.action(\.+)newUserInfo url script-response-body https://raw.githubusercontent.com/Epoch992/QuantumultX/QX/Script/JDwskey.js
@@ -5,3 +7,4 @@
 [mitm]
 
 hostname = api.m.jd.com
+****/
